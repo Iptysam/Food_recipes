3a4b09e1ed241434d612223bf1763ee3ed5785ee
Iptysam <mukadamiptisam@gmail.com>
2022-09-17 13:58:39 +0300

    Update

--- about.js
+++ about.js
@@ -4,12 +4,12 @@ const navlinks = document.querySelectorAll('.nav-links');
 const closeBtn = document.querySelector('.close_btn');
 
 function open() {
-    hamburger.classList.toggle('active');
-    Menu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  Menu.classList.toggle('active');
   }
   
-  hamburger.addEventListener('click', open);
-  closeBtn.addEventListener('click', open);
-  navlinks.forEach((link) => {
-    link.addEventListener('click', open);
-  });
+hamburger.addEventListener('click', open);
+closeBtn.addEventListener('click', open);
+navlinks.forEach((link) => {
+  link.addEventListener('click', open);
+});
--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const navlinks = document.querySelectorAll('.nav-link');
 const closeBtn = document.querySelector('.close_btn');
 
 function open() {
-    hamburger.classList.toggle('active');
-    Menu.classList.toggle('active');
-  }
-  
-  hamburger.addEventListener('click', open);
-  closeBtn.addEventListener('click', open);
-  navlinks.forEach((link) => {
-    link.addEventListener('click', open);
-  });
+  hamburger.classList.toggle('active');
+  Menu.classList.toggle('active');
+}
+
+hamburger.addEventListener('click', open);
+closeBtn.addEventListener('click', open);
+navlinks.forEach((link) => {
+  link.addEventListener('click', open);
+});
  
-const feature_card = [{
+const Feature_card = [{
   title: 'Rasmalai',
   picimg: 'images/rasmalai.webp',
   sdescrip: 'indian sweet dish',
@@ -24,7 +24,7 @@ const feature_card = [{
   class: 'block1',
 },
 {
-	title: 'Biryani',
+  title: 'Biryani',
 	picimg: 'images/biryani.jpg',
 	sdescrip: 'famous indian main dish',
   lineimg: 'images/minus.png',
@@ -33,45 +33,45 @@ const feature_card = [{
 	class: 'block2',
 },
 {
-title: 'Rasmalai',
-picimg: 'images/rasmalai.webp',
-sdescrip: 'indian sweet dish',
-lineimg: 'images/minus.png',
-ldescrip: 'One of the best desserts that I love and easy to make.',
-liveLink: 'https://iptysam.github.io/The_Portfolio/',
-class: 'block1',
+  title: 'Rasmalai',
+  picimg: 'images/rasmalai.webp',
+  sdescrip: 'indian sweet dish',
+  lineimg: 'images/minus.png',
+  ldescrip: 'One of the best desserts that I love and easy to make.',
+  liveLink: 'https://iptysam.github.io/The_Portfolio/',
+  class: 'block1',
 },
 {
-title: 'Biryani',
-picimg: 'images/biryani.jpg',
-sdescrip: 'famous indian main dish',
-lineimg: 'images/minus.png',
-ldescrip: 'Mostly a festival food especially in Eid holiday.',
-liveLink: 'https://iptysam.github.io/The_Portfolio/',
-class: 'block2',
+  title: 'Biryani',
+  picimg: 'images/biryani.jpg',
+  sdescrip: 'famous indian main dish',
+  lineimg: 'images/minus.png',
+  ldescrip: 'Mostly a festival food especially in Eid holiday.',
+  liveLink: 'https://iptysam.github.io/The_Portfolio/',
+  class: 'block2',
 },
 {
-title: 'Rasmalai',
-picimg: 'images/rasmalai.webp',
-sdescrip: 'indian sweet dish',
-lineimg: 'images/minus.png',
-ldescrip: 'One of the best desserts that I love and easy to make.',
-liveLink: 'https://iptysam.github.io/The_Portfolio/',
-class: 'block1',
+  title: 'Rasmalai',
+  picimg: 'images/rasmalai.webp',
+  sdescrip: 'indian sweet dish',
+  lineimg: 'images/minus.png',
+  ldescrip: 'One of the best desserts that I love and easy to make.',
+  liveLink: 'https://iptysam.github.io/The_Portfolio/',
+  class: 'block1',
 },
 {
-title: 'Biryani',
-picimg: 'images/biryani.jpg',
-sdescrip: 'famous indian main dish',
-lineimg: 'images/minus.png',
-ldescrip: 'Mostly a festival food especially in Eid holiday.',
-liveLink: 'https://iptysam.github.io/The_Portfolio/',
-class: 'block2',
+  title: 'Biryani',
+  picimg: 'images/biryani.jpg',
+  sdescrip: 'famous indian main dish',
+  lineimg: 'images/minus.png',
+  ldescrip: 'Mostly a festival food especially in Eid holiday.',
+  liveLink: 'https://iptysam.github.io/The_Portfolio/',
+  class: 'block2',
 },
-]
+];
 
 const cards = document.querySelector('.featurecard');
-feature_card.forEach((card) => {
+Feature_card.forEach((card) => {
   cards.innerHTML += `
   <div class="block1 c-card">
   <img src="${card.picimg}" alt="snaphot" class="p-img">
