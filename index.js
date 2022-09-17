c948172344207dec97667149195b519b8644c666
Iptysam <mukadamiptisam@gmail.com>
2022-09-17 14:51:23 +0300

    Update js

--- index.js
+++ index.js
@@ -4,74 +4,74 @@ const navlinks = document.querySelectorAll('.nav-link');
 const closeBtn = document.querySelector('.close_btn');
 
 function open() {
-	hamburger.classList.toggle('active');
-	Menu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  Menu.classList.toggle('active');
 }
 
 hamburger.addEventListener('click', open);
 closeBtn.addEventListener('click', open);
 navlinks.forEach((link) => {
-	link.addEventListener('click', open);
+  link.addEventListener('click', open);
 });
 
-const feature_Card = [{
-	title: 'Rasmalai',
-	picimg: 'images/rasmalai.webp',
-	sdescrip: 'indian sweet dish',
-	lineimg: 'images/minus.png',
-	ldescrip: 'One of the best desserts that I love and easy to make.',
-	liveLink: 'https://iptysam.github.io/The_Portfolio/',
-	class: 'block1',
+const featureCard = [{
+  title: 'Rasmalai',
+  picimg: 'images/rasmalai.webp',
+  sdescrip: 'indian sweet dish',
+  lineimg: 'images/minus.png',
+  ldescrip: 'One of the best desserts that I love and easy to make.',
+  liveLink: 'https://iptysam.github.io/The_Portfolio/',
+  class: 'block1',
 },
 {
-	title: 'Biryani',
+  title: 'Biryani',
   picimg: 'images/biryani.jpg',
   sdescrip: 'famous indian main dish',
-	lineimg: 'images/minus.png',
+  lineimg: 'images/minus.png',
   ldescrip: 'Mostly a festival food especially in Eid holiday.',
   liveLink: 'https://iptysam.github.io/The_Portfolio/',
   class: 'block2',
 },
 {
-	title: 'Rasmalai',
-	picimg: 'images/rasmalai.webp',
-	sdescrip: 'indian sweet dish',
-	lineimg: 'images/minus.png',
-	ldescrip: 'One of the best desserts that I love and easy to make.',
-	liveLink: 'https://iptysam.github.io/The_Portfolio/',
-	class: 'block1',
+  title: 'Rasmalai',
+  picimg: 'images/rasmalai.webp',
+  sdescrip: 'indian sweet dish',
+  lineimg: 'images/minus.png',
+  ldescrip: 'One of the best desserts that I love and easy to make.',
+  liveLink: 'https://iptysam.github.io/The_Portfolio/',
+  class: 'block1',
 },
 {
-	title: 'Biryani',
-	picimg: 'images/biryani.jpg',
-	sdescrip: 'famous indian main dish',
-	lineimg: 'images/minus.png',
-	ldescrip: 'Mostly a festival food especially in Eid holiday.',
-	liveLink: 'https://iptysam.github.io/The_Portfolio/',
-	class: 'block2',
+  title: 'Biryani',
+  picimg: 'images/biryani.jpg',
+  sdescrip: 'famous indian main dish',
+  lineimg: 'images/minus.png',
+  ldescrip: 'Mostly a festival food especially in Eid holiday.',
+  liveLink: 'https://iptysam.github.io/The_Portfolio/',
+  class: 'block2',
 },
 {
-	title: 'Rasmalai',
-	picimg: 'images/rasmalai.webp',
-	sdescrip: 'indian sweet dish',
-	lineimg: 'images/minus.png',
-	ldescrip: 'One of the best desserts that I love and easy to make.',
-	liveLink: 'https://iptysam.github.io/The_Portfolio/',
-	class: 'block1',
+  title: 'Rasmalai',
+  picimg: 'images/rasmalai.webp',
+  sdescrip: 'indian sweet dish',
+  lineimg: 'images/minus.png',
+  ldescrip: 'One of the best desserts that I love and easy to make.',
+  liveLink: 'https://iptysam.github.io/The_Portfolio/',
+  class: 'block1',
 },
 {
-	title: 'Biryani',
-	picimg: 'images/biryani.jpg',
-	sdescrip: 'famous indian main dish',
-	lineimg: 'images/minus.png',
-	ldescrip: 'Mostly a festival food especially in Eid holiday.',
+  title: 'Biryani',
+  picimg: 'images/biryani.jpg',
+  sdescrip: 'famous indian main dish',
+  lineimg: 'images/minus.png',
+  ldescrip: 'Mostly a festival food especially in Eid holiday.',
   liveLink: 'https://iptysam.github.io/The_Portfolio/',
   class: 'block2',
 },
 ];
 
 const cards = document.querySelector('.featurecard');
-feature_Card.forEach((card) => {
+featureCard.forEach((card) => {
   cards.innerHTML += `
   <div class="block1 c-card">
   <img src="${card.picimg}" alt="snaphot" class="p-img">
